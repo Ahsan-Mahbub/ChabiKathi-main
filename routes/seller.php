1ce8aa71218b8d28d5bd556d8e9a7c5c5c8ab7a9
meheraj1999 <mehrajemon1999@gmail.com>
2021-08-25 00:25:06 +0600

    add

---
+++ routes/seller.php
@@ -0,0 +1,12 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Seller\SellerController;
+
+
+
+Route::group(['namespace' => 'seller', 'as' => 'seller.'], function () {
+Route::get('sellercheck', [SellerController::class, 'index'])->name('seller');
+
+});
+
